700bb4988a5509ce05337bce0a4b7a511e5ab500
rverma510 <raghavverma4091@gmail.com>
2020-07-18 09:16:45 +0530

    completed return os method

--- src/system.cpp
+++ src/system.cpp
@@ -27,7 +27,7 @@ std::string System::Kernel() { return LinuxParser::Kernel(); }
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
